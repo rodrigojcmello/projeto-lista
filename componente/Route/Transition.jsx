9dd5b14a543424595a464883efa4d78ebd0955f4
Rodrigo Mello <rodrigojcmello@gmail.com>
2017-03-18 23:28:18 -0300

    ajustes

--- componente/Route/Transition.jsx
+++ componente/Route/Transition.jsx
@@ -21,7 +21,8 @@ export default class extends Component {
             transitionLeaveTimeout: 501,
             origem: {
                 "/pagina1": "transition-slide-left",
-                "/pagina3": "transition-slide-right"
+                "/pagina3": "transition-slide-right",
+                "/pagina3/pagina4": "transition-slide-right"
             }
         };
         rota["/pagina3"] = {
@@ -38,18 +39,26 @@ export default class extends Component {
         var transitionLeaveTimeout = 2;
 
         if (typeof this.props.location != "undefined") {
-            if (typeof this.props.location.pathname != "undefined") {
-                var pathname = this.props.location.pathname;
-                if (rota[pathname]) {
-                    if (typeof global.historico[0] != "undefined") {
+            var pathname = this.props.location.pathname;
+            if (rota[pathname]) {
+                if (typeof global.historico[0] != "undefined") {
+                    if (typeof rota[pathname].origem[global.historico[0]] != "undefined") {
                         transitionName = rota[pathname].origem[global.historico[0]];
+                    } else {
+                        var p, pos = global.historico[0].indexOf("/");
+                        while (pos > -1) {
+                            p = pos;
+                            pos = global.historico[0].indexOf("/", pos + 1);
+                        }
+                        transitionName = rota[pathname].origem[global.historico[0].substring(0, p)];
                     }
-                    transitionEnterTimeout = rota[pathname].transitionEnterTimeout;
-                    transitionLeaveTimeout = rota[pathname].transitionLeaveTimeout;
                 }
-                // console.log("estou em: " + pathname);
-                // console.log("estava em: " + global.historico[0]);
-                // console.log("-----");
+                transitionEnterTimeout = rota[pathname].transitionEnterTimeout;
+                transitionLeaveTimeout = rota[pathname].transitionLeaveTimeout;
+
+                console.log("estou em: " + pathname);
+                console.log("estava em: " + global.historico[0]);
+                console.log("-----");
             }
         }
 
